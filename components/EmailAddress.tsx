a063a8b7aad80ee3705d206bd3d408eac21b4482
Getter Hiss <gh@getter.dev>
2021-12-30 13:40:55 -0800

    Obfuscate Email Address

---
+++ components/EmailAddress.tsx
@@ -0,0 +1,24 @@
+import he from 'he'
+
+type EmailAddressProps = {
+  className: string;
+  name: string;
+  domain: string;
+  tld: string;
+}
+
+const EmailAddress = ({className, name, domain, tld}: EmailAddressProps) => {
+  const tmpl = `
+    <a
+      class="${className}"
+      href="${he.encode(`mailto:${name}@${domain}.${tld}`, { 'encodeEverything': true })}" 
+      target="_blank"
+    >
+      ${he.encode(`${name}@${domain}.${tld}`, { 'encodeEverything': true })}
+    </a>`;
+
+  return (
+    <div dangerouslySetInnerHTML={{__html: tmpl}} />
+  )
+}
+export default EmailAddress;
