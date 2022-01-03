c207e7e0cd51b03153fd11a98ad1172256210acc
Getter Hiss <gh@getter.dev>
2022-01-03 13:28:04 -0800

    Added Stripe, Twilio and Vercel SVG Logos

--- icons/index.tsx
+++ icons/index.tsx
@@ -5,8 +5,11 @@ import GraphQL from './GraphQL';
 import Next from './Next';
 import React from './React';
 import Redux from './Redux';
+import Stripe from './Stripe';
 import Tailwind from './Tailwind';
+import Twilio from './Twilio';
 import TypeScript from './TypeScript';
+import Vercel from './Vercel';
 
 export {
   Android,
@@ -16,6 +19,9 @@ export {
   Next,
   React,
   Redux,
+  Stripe,
   Tailwind,
+  Twilio,
   TypeScript,
+  Vercel,
 };
